4537c717a572af3f39df6cf8da866632312af10f
Bajracharya <bajracj@38f9d3dac8ef.ant.amazon.com>
2019-11-08 15:03:16 -0800

    question three testadded

--- app.js
+++ app.js
@@ -61,11 +61,26 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
+  var sum = sumThreeNums(a, b, c)
+  var product = multiplyThreeNums(a, b, c);
+  var sumSent = `${a} and ${b} and ${c} sum to ${sum}.`;
+  var productSent = `The product of ${a} and ${b} and ${c} is ${product}.`;
+  var arr = [sum, product, sumSent, productSent];
+  return arr;
+}
 
+function sumThreeNums(a, b, c) {
+  var sum = a + b + c;
+  return sum;
+}
+
+function multiplyThreeNums(a, b, c) {
+  var product = a * b * c;
+  return product;
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
