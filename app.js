3d44abc31cb7fc99ae6cca33aef06895616b82c6
Bajracharya <bajracj@38f9d3dac8ef.ant.amazon.com>
2019-11-08 21:18:11 -0800

    question five test completed

--- app.js
+++ app.js
@@ -88,11 +88,10 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 var testArray = [2, 3, 4]; //eslint-disable-line
 
-function sumArray(sumArr) { //eslint-disable-line  
-    
-    var total = sum(sum(testArray[0], testArray[1])[0], testArray[2])[0];
+function sumArray(sumArr) { //eslint-disable-line
+    var total = sum(sum(sumArr[0], sumArr[1])[0], sumArr[2])[0];
     //console.log(total);
-    var totalSent = `${testArray[0]},${testArray[1]},${testArray[2]} was passed in as an array of numbers, and ${total} is their sum.`
+    var totalSent = `${sumArr[0]},${sumArr[1]},${sumArr[2]} was passed in as an array of numbers, and ${total} is their sum.`
     var arr = [total, totalSent];
   return arr;
 }
@@ -115,11 +114,14 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
-
+  var total = multiply(multiply(multArr[0],multArr[1])[0], multArr[2])[0];
+  var totalSent = `The numbers ${multArr[0]},${multArr[1]},${multArr[2]} have a product of ${total}.`;
+  var arr = [total, totalSent];
+  return arr;
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
